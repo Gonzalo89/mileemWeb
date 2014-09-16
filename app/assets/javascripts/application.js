1628e1ac2679859d2805903737fc3719db756684
Facundo <rodrif89@gmail.com>
2014-09-15 23:51:11 -0300

    problemas con el geocoder

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,98 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+var map;
+var markers = [];
+var mapClickListener;
+var mapDragListener;
+
+function initialize() {
+  var ubicacionPersona = new google.maps.LatLng(-34.64104659419889, -58.56584072113037);  
+  var idPersona = document.getElementsByName('idPersona')[0];
+  
+  if (idPersona.value != -1) {
+    var lat = document.getElementsByName("latitude")[0];
+    var lng = document.getElementsByName("longitude")[0];
+    ubicacionPersona = new google.maps.LatLng(lat.value, lng.value);
+  }
+  
+  var mapOptions = {
+    zoom: 12,
+    center: ubicacionPersona,
+    mapTypeId: google.maps.MapTypeId.TERRAIN
+  };
+
+  map = new google.maps.Map(document.getElementById('mapa'), mapOptions);
+
+  // Adds a marker at the center of the map.
+  addMarker(ubicacionPersona);
+}
+
+function AddMapHandlers()
+{
+mapClickListener = google.maps.event.addListener(map, 'click', function(event) {
+    addMarker(event.latLng);
+    RemoveMapHandlers();
+    AddMapHandlers();
+  });
+mapDragkListener = google.maps.event.addListener(markers[0], 'dragend', function(event) {
+//   alert("drag end");
+    addMarker(event.latLng);
+    RemoveMapHandlers();
+    AddMapHandlers();
+  });
+markers[0].setDraggable(true);
+}
+
+function RemoveMapHandlers()
+{
+mapClickListener.remove();
+mapDragkListener.remove();
+markers[0].setDraggable(false);
+}
+
+// Add a marker to the map and push to the array.
+function addMarker(location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  
+  deleteMarkers();
+  markers.push(marker);
+  setLocation(location);
+}
+
+function setLocation(location)
+{
+        var inputLat = document.getElementsByName('Latitud')[0]; 
+        inputLat.value = location.lat();
+        var inputLng = document.getElementsByName('Longitud')[0]; 
+        inputLng.value = location.lng();
+}
+
+// Sets the map on all markers in the array.
+function setAllMap(map) {
+  for (var i = 0; i < markers.length; i++) {
+    markers[i].setMap(map);
+  }
+}
+
+// Removes the markers from the map, but keeps them in the array.
+function clearMarkers() {
+  setAllMap(null);
+}
+
+// Shows any markers currently in the array.
+//function showMarkers() {
+//  setAllMap(map);
+//}
+
+// Deletes all markers in the array by removing references to them.
+function deleteMarkers() {
+  clearMarkers();
+  markers = [];
+}
+
+google.maps.event.addDomListener(window, 'load', initialize);

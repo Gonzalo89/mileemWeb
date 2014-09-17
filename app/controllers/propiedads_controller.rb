cff804799d47c0b6e3195e891bbf9c03c4caaa8d
Facundo <rodrif89@gmail.com>
2014-09-17 12:00:49 -0300

    [WUS017]

--- app/controllers/propiedads_controller.rb
+++ app/controllers/propiedads_controller.rb
@@ -89,7 +89,7 @@ class PropiedadsController < ApplicationController
       format.json { head :no_content }
     end
   end
-
+  
   private
 
   # Use callbacks to share common setup or constraints between actions.
--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -1,8 +1,8 @@
 require 'carrierwave/orm/activerecord'
 
 class Propiedad < ActiveRecord::Base
-  geocoded_by :direccion  
-  after_validation :geocode, :if => :direccion_changed?
+  geocoded_by :direccion_completa
+  after_validation :geocode
   validates :direccion, presence: true
   validates :numero, numericality: { only_integer: true, less_than_or_equal_to: 999999999 }
   validates :piso, length: { maximum: 10 }
@@ -15,13 +15,17 @@ class Propiedad < ActiveRecord::Base
   validates :superficie, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :ambientes, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :superficie_nc, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-   
+
   has_many :fotos, dependent: :destroy
-  has_many :tieneamenities, dependent: :destroy  
-  has_many :amenities, :through => :tieneamenities  
-  
+  has_many :tieneamenities, dependent: :destroy
+  has_many :amenities, :through => :tieneamenities
+
   belongs_to :barrio
   belongs_to :tipo_propiedad
   belongs_to :moneda
-  belongs_to :operacion  
+  belongs_to :operacion
+  
+  def direccion_completa
+    [direccion, numero].compact.join(' ')
+  end     
 end

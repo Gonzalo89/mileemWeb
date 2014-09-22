7d585cd62fdc6d2ad832c673ba83c85ddf7cee1b
Agustín Mezzina <apmezzina@gmail.com>
2014-09-22 12:54:32 -0300

    agregadas restricciones de positividad

--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -6,13 +6,13 @@ class Propiedad < ActiveRecord::Base
   validates :piso, length: { maximum: 10 }
   validates :departamento, length: { maximum: 10 }
   validates :descripcion, length: { minimum: 10, maximum: 500 }
-  validates :precio, numericality: { only_integer: true, greater_than: 1, less_than_or_equal_to: 999999999 }
-  validates :antiguedad, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :dormitorios, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :expensas, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :superficie, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :ambientes, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :superficie_nc, numericality: {only_integer: true, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :precio, numericality: { only_integer: true, greater_than: 0, less_than_or_equal_to: 999999999 }
+  validates :antiguedad, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :dormitorios, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :expensas, numericality: {only_integer: true, greater_than: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :superficie, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :ambientes, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
+  validates :superficie_nc, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
    
   has_many :fotos, dependent: :destroy
   has_many :tieneamenities, dependent: :destroy  

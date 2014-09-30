d0914001fc7000d562f80ac9bc9a7600c7a2eb27
Facundo <rodrif89@gmail.com>
2014-09-30 14:14:47 -0300

    [WUS019] [RQ1] pasan los test anteriores

--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -16,7 +16,7 @@ class Propiedad < ActiveRecord::Base
   validates :superficie, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :ambientes, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :superficie_nc, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :fecha_publicacion, :date => {:after => Proc.new { Time.now }, :before => Proc.new { Time.now + 1.year } }
+  validates :fecha_publicacion, presence: true, :date => {:after => Proc.new { Time.now } }
    
   has_many :fotos, dependent: :destroy
   has_many :tieneamenities, dependent: :destroy
--- test/controllers/propiedads_controller_test.rb
+++ test/controllers/propiedads_controller_test.rb
@@ -35,7 +35,8 @@ class PropiedadsControllerTest < ActionController::TestCase
     assert_difference('Propiedad.count') do
       post :create, propiedad: { barrio_id: @propiedad.barrio_id, direccion: @propiedad.direccion,
         numero: @propiedad.numero, descripcion: @propiedad.descripcion, moneda_id: @propiedad.moneda_id,
-        precio: @propiedad.precio, operacion_id: @propiedad.operacion_id, user_id: @user.id}
+        precio: @propiedad.precio, operacion_id: @propiedad.operacion_id, user_id: @user.id,
+        tipo_publicacion_id: @propiedad.tipo_publicacion_id, fecha_publicacion: @propiedad.fecha_publicacion}
     end
 
     assert_redirected_to propiedad_path(assigns(:propiedad))

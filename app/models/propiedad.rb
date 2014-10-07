b334264e1bf7bd99d4a2226ae4a818ad56eee606
Facundo <rodrif89@gmail.com>
2014-10-07 11:10:42 -0300

    [BUG62] arreglado

--- app/models/propiedad.rb
+++ app/models/propiedad.rb
@@ -16,7 +16,7 @@ class Propiedad < ActiveRecord::Base
   validates :superficie, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :ambientes, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
   validates :superficie_nc, numericality: {only_integer: true, greater_than_or_equal_to: 0, less_than_or_equal_to: 999999999 }, allow_blank: true
-  validates :fecha_publicacion, presence: true
+  validates :fecha_publicacion, presence: true, :date => {:after => Time.new(Time.now.year, Time.now.month, Time.now.day-1)  }, :on => :create
    
   has_many :fotos, dependent: :destroy
   has_many :tieneamenities, dependent: :destroy

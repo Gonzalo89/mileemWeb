57009aeda4a0413403af2605cbddf27e0ccab140
elian1985 <elian1985@gmail.com>
2014-11-19 16:30:15 -0300

    [AUS016] Subo pagina que muestra grafico de torta (pie chart)

--- config/routes.rb
+++ config/routes.rb
@@ -34,6 +34,8 @@ Rails.application.routes.draw do
   resources :barrios
 
   get 'welcome/index'
+  
+  get '/piechart' => 'piechart#piechart'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".

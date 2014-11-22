90696bbcd5c35eee8e2675328e649b00fed3d430
elian1985 <elian1985@gmail.com>
2014-11-22 00:33:22 -0300

    [AUS016] Agrego gráfico de column chart para pruebas

--- config/routes.rb
+++ config/routes.rb
@@ -37,6 +37,7 @@ Rails.application.routes.draw do
   
   get '/piechart' => 'piechart#piechart'
   get '/columnchart' => 'columnchart#columnchart'
+  get '/columnchartprueba' => 'columnchartprueba#columnchartprueba'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
